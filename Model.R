99016ae256724f0f4980993c9ffae7033aa67f66
Anmol Desai <Anmol@Anmols-MacBook-Pro.local>
2017-04-04 19:55:11 -0400

    Parameters

--- Model.R
+++ Model.R
@@ -25,4 +25,31 @@ SEIRMosVec = function(time, state, parms) {
   })
 }
 
+# states and parameters
 
+b1 #number of Aedes eggs laid per day#
+N1 #size of mosquito population# 
+q1 #transovarial transmission rate in Aedes#
+I1 #Infectious in mosquitoes#
+theta1 #1/theta1 = development time of Aedes#
+P1 #uninfected Aedes eggs#
+Q1 #infected Aedes eggs#
+d1 #1/d1 = lifespand of Aedes#
+S1 #Aedes susceptibles# 
+beta.M #Beta21 - adequate contact rate from Aedes to livestock#
+I2 #Infectious in livestock#
+N2 #total livestock population size#
+E1 #Aedes exposed#
+epsilon1 #1/epsilon1 = incubation period in livestock#
+b2 #daily birthrate in livestock# 
+d2 #1/d2 = lifespan of livestock animals# 
+S2 #livestock susceptibles#
+beta.L #Beta12 - adequate contact rate from livestock to Aedes#
+E2 #livestock exposed#
+epsilon2 #1/epsilon2 = incubation period in Aedes#
+gamma2 #1/gamma2 = infectiousness period in livestock#
+mu2 #RVF mortality rate in livestock#
+R2 #infected livestock death or recovery with immunity from RVF#
+
+
+  
