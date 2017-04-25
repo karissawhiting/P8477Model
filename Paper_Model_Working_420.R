49799c2dbd21102e12af916cc4dc5b8390383eb2
Karissa <whiting@ecohealthalliance.org>
2017-04-25 18:10:52 -0400

    prof changes

--- Paper_Model_Working_420.R
+++ Paper_Model_Working_420.R
@@ -19,18 +19,21 @@ SEIRMosVec = function(time, state, parms) {
     dIL = alphaL*EL - gamma*IL - muL*IL - dL*IL
     dInci = alphaL*EL
 
-    list(c(dSL,dIL, dSM, dIM, dEL, dEM, dP, dQ, dInci))
+    list(c(dP, dQ, dSM, dEM, dIM, dSL,dEL, dIL, dInci))
+           #dIL, dSM, dIM, dEL, dEM, dP, dQ, dInci))
   })
 }
 
+#state = c(SL = SL, IL = IL, SM = SM,IM = IM, EL = EL, EM = EM, P = P, Q = Q, Inci = 0)
 ######################
 # State Variables    #
 ######################
 #Egg
-P = 999;
-Q = 1;
+P = 5000# 999;
+Q = 0;
 #Mosquito
-NM=1; IM=1; SM=NM-IM;
+NM=5000; 
+IM=1; SM=NM-IM;
 EM= 0;
 #Livestock
 NL=1000; IL =0; 
@@ -42,28 +45,29 @@ SL=NL-IL-EL;
 # Parameters         #
 ######################
 #Mosquito
-b1 = 1/30;       #number of eggs laid per day
+b1 = 1/20;       #number of eggs laid per day
 q1 = .05;        #transovarial transmission rate
 theta = (1/10);  #maturation rate
 alphaM = 1/6;    #transition from exposed to infected mosquitos
-muM = 1/10;      #Mosquito death rate, 1 week life span for mosquito
-BML = .1         #transmission rate to mosquito from livestock
+muM = 1/20;      #Mosquito death rate, 1 week life span for mosquito
+BML = .2762       #transmission rate to mosquito from livestock
 
 #Livestock
 alphaL=(1/4);     #transition from exposed to infected livestock
-muL= (1/1000);    #livestock death rate, life span: 10 yr
-gamma= (1/3);     # recovery rate livestock, infectious period: 7 days
+muL= (1/10/365);    #livestock death rate, life span: 10 yr
+gamma= (1/5);     # recovery rate livestock, infectious period: 7 days
 #vL=NL*muL;       #birth rate livestock
-BLM= .1           #transmission rate to livestock from mosquito
+BLM= BML           #transmission rate to livestock from mosquito
 dL = .07          #death rate in livestock due to RVF
 
 parameters = c(muL = muL, muM = muM, 
                gamma = gamma, alphaL = alphaL, alphaM = alphaM, 
                theta = theta, b1 = b1, q1 = q1, dL = dL, BML = BML, BLM = BLM)
 
-state = c(SL = SL, IL = IL, SM = SM,IM = IM, EL = EL, EM = EM, P = P, Q = Q, dInci = 0)
+state = c(SL = SL, IL = IL, SM = SM,IM = IM, EL = EL, EM = EM, P = P, Q = Q, Inci = 0)
+state = c(P=P, Q=Q, SM=SM, EM=EM, IM=IM, SL=SL, EL=EL, IL=IL, Inci=0)
 
-times=1:(365*1); # months
+times=1:(365*10); # months
 
 ######################
 # Simulation         #
