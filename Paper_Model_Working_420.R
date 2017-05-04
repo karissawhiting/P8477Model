b06553500c1d4b6c09651f10650ac139e23d81b8
Karissa <whiting@ecohealthalliance.org>
2017-05-04 10:36:58 -0400

    all updates

--- Paper_Model_Working_420.R
+++ Paper_Model_Working_420.R
@@ -2,7 +2,7 @@ library(deSolve)
 library(ggplot2)
 library(reshape2)
 library(gridExtra)
-
+library(dplyr)
 # function for a simple mosquito borne disease
 SEIRMosVec = function(time, state, parms) {
   with(as.list(c(state, parms)), {
@@ -44,30 +44,33 @@ SL=NL-IL-EL;
 ######################
 # Parameters         #
 ######################
+
+#NOTE, if we held b1 = theta = MuM, we would have P+Q+NM be constant over time. We are letting it vary
+
 #Mosquito
-b1 = 1/20;       #number of eggs laid per day
-q1 = .05;        #transovarial transmission rate
-theta = (1/10);  #maturation rate
+b1 = 1/31.5;       #number of eggs laid per day
+q1 = .1;        #transovarial transmission rate
+theta = (1/15);  #maturation rate
 alphaM = 1/6;    #transition from exposed to infected mosquitos
-muM = 1/20;      #Mosquito death rate, 1 week life span for mosquito
-BML = .2762       #transmission rate to mosquito from livestock
+muM = 1/31.5;      #Mosquito death rate, 1 week life span for mosquito
+BML = .2429      #transmission rate to mosquito from livestock
 
 #Livestock
-alphaL=(1/4);     #transition from exposed to infected livestock
-muL= (1/10/365);    #livestock death rate, life span: 10 yr
-gamma= (1/5);     # recovery rate livestock, infectious period: 7 days
+alphaL=.58;     #transition from exposed to infected livestock
+muL= (1/1980);    #livestock death rate, approx 5.5 years (average)
+gamma= .6;     # recovery rate livestock, infectious period: 7 days
 #vL=NL*muL;       #birth rate livestock
-BLM= BML           #transmission rate to livestock from mosquito
-dL = .07          #death rate in livestock due to RVF
+BLM= 0.2762           #transmission rate to livestock from mosquito
+dL = .0625         #death rate in livestock due to RVF
 
 parameters = c(muL = muL, muM = muM, 
                gamma = gamma, alphaL = alphaL, alphaM = alphaM, 
                theta = theta, b1 = b1, q1 = q1, dL = dL, BML = BML, BLM = BLM)
 
-state = c(SL = SL, IL = IL, SM = SM,IM = IM, EL = EL, EM = EM, P = P, Q = Q, Inci = 0)
+#state = c(SL = SL, IL = IL, SM = SM,IM = IM, EL = EL, EM = EM, P = P, Q = Q, Inci = 0)
 state = c(P=P, Q=Q, SM=SM, EM=EM, IM=IM, SL=SL, EL=EL, IL=IL, Inci=0)
 
-times=1:(365*10); # months
+times=1:(365*15); # months
 
 ######################
 # Simulation         #
@@ -80,8 +83,102 @@ sim=ode(y=state,times=times,func=SEIRMosVec,parms = parameters)
 # Plots              #
 ######################
 plot(sim)
+df
+#new<- mutate(simdf, NM = (SM + EM + IM+P+Q))
+#plot(new$NM)
+
+simdf<- as.data.frame(sim)
+simdf2<- simdf %>%
+  rename("Uninfected Eggs" = P, "Infected Eggs" = Q, 
+         "Susceptible Mosquitos" = SM, "Exposed Mosquitos" = EM, "Infected Mosquitos" =IM,
+         "Susceptible Livestock" = SL, "Exposed Livestock" = EL, "Infectious Livestock" = IL,"Cumulative Inci" = Inci) 
+
+simdf2melt<- simdf2 %>% select(`Susceptible Mosquitos`, `Exposed Mosquitos`,`Infected Mosquitos`, 
+                               `Susceptible Livestock`, `Exposed Livestock`, `Infectious Livestock`, time) %>% melt(., id = 'time')
+
+jpeg('dynamics.jpeg', units="in", width=8, height=4, res=300)
+ggplot(data=simdf2melt, aes(x=time, y= value, group=variable, colour=factor(variable))) + 
+  geom_line(size=.5) + ylim(c(0,6500)) + xlim(c(0, 400)) + ylab("Number of Individuals")
+dev.off()
+######################
+
+#totmost <- simdf2 %>% 
+  #select(time, `Infected Eggs`, `Susceptible Mosquitos`, `Exposed Mosquitos`, `Infected Mosquitos`, `Uninfected Eggs`, `Infected Eggs`) %>% 
+  #mutate(`Total Mosquito` = `Susceptible Mosquitos` + `Infected Mosquitos`) %>% 
+  #mutate(`Total Eggs` = `Uninfected Eggs` + `Infected Eggs`) %>%
+  #mutate(All = `Total Eggs` + `Total Mosquito`)
+#totmostmelt <- totmost %>% melt(., id = 'time')
+
+#################################
+#TOTAL EGG PLOTS
+totegg<- totmost %>% select(`Infected Eggs`, `Uninfected Eggs`, `Total Eggs`, time)
+toteggmelt<- melt(totegg, id = 'time')
+
+jpeg('toteggs.jpeg', units="in", width=15, height=5, res=300)
+ptotegg<- ggplot(toteggmelt, aes(time, value)) + 
+  geom_line(size = 1, color = "light blue") + 
+  facet_wrap(~variable, scales="free_y") + 
+  xlab("Time (Days)") + ylab("Number of Eggs") +
+  theme_minimal() + theme(strip.text = element_text(size=15))
+ptotegg
+dev.off()
+
+filter(totegg, totegg$`Infected Eggs` > 15)
+totegg[3100:3500,]
+#################################
+# TOTAL MOSQUITO PLOTS
+totmos<- totmost %>% select(`Susceptible Mosquitos`, `Exposed Mosquitos`, `Infected Mosquitos`, `Total Mosquito`, time)
+totmosmelt<- melt(totmos, id = 'time')
+
+jpeg('totmosquit.jpeg', units="in", width=15, height=10, res=300)
+ptotmos<- ggplot(totmosmelt, aes(time, value)) + 
+  geom_line(size = 1, color = "pink") + 
+  facet_wrap(~variable, scales="free_y") + 
+  xlab("Time (Days)") + ylab("Number of Mosquitos") +
+  theme_minimal() + theme(strip.text = element_text(size=15))
+ptotmos
+dev.off()
+
+totmos2<- totmos %>% mutate(., EnM = `Exposed Mosquitos` + `Infected Mosquitos`)
+tail(totmos2)
+totmos2[170:222,]
+
+##################
+###TOTAL LIVESTOCK PLOTS
+
+totliv <- simdf2 %>% 
+  select(time, `Susceptible Livestock`, `Exposed Livestock`, `Infectious Livestock`, `Cumulative Inci`)
+
+totlivmel <- totliv %>% melt(., id = 'time')
+
+#jpeg('ptotliv.jpeg', units="in", width=15, height=10, res=300)
+ptotliv<- ggplot(totlivmel, aes(time, value)) + 
+  geom_line(size = 1, color = "light green") + 
+  facet_wrap(~variable, scales="free_y") + 
+  xlab("Time (Days)") + ylab("Number of Livestock") +
+  theme_minimal() + theme(strip.text = element_text(size=15))
+ptotliv
+#dev.off()
+
+tail(totliv)
+
+totliv[150:250,]
+summarise(totliv$`Infected Livestock`)
+
+ggplot(data=totmostmelt, aes(x=time, y= value, group=variable, colour=factor(variable))) + 
+  geom_line(size=.5) + ylim(c(0,1000)) + xlim(c(0, 1000))
+
+ggplot(data=simdf4, aes(x=time, y= value, group=variable, colour=factor(variable))) + 
+  geom_line(size=.5) + ylim(c(0,1000)) + xlim(c(0, 1000))
+###################
 
 
+p3<- ggplot(simdf2, aes(time, value)) + 
+  geom_line(size = 1, color = "navy blue") + 
+  facet_wrap(~variable, scales="free_y") + 
+  xlab("Time (Days)") + ylab("Number of Livestock") +
+  theme_minimal() + theme(strip.text = element_text(size=12))
+p3
 ######################
 # R0             #
 ######################
--- culling_experiment.R
+++ culling_experiment.R
@@ -30,7 +30,7 @@ SEIRMosVec = function(time, state, parms) {
 # State Variables    #
 ######################
 #Egg
-P = 1000# 999;
+P = 5000# 999;
 Q = 0;
 #Mosquito
 NM=5000; 
@@ -45,58 +45,102 @@ SL=NL-IL-EL;
 ######################
 # Parameters         #
 ######################
+
+#NOTE, if we held b1 = theta = MuM, we would have P+Q+NM be constant over time. We are letting it vary
+
 #Mosquito
-b1 = 1/20;       #number of eggs laid per day
-q1 = .05;        #transovarial transmission rate
-theta = (1/10);  #maturation rate
+b1 = 1/31.5;       #number of eggs laid per day
+q1 = .1;        #transovarial transmission rate
+theta = (1/15);  #maturation rate
 alphaM = 1/6;    #transition from exposed to infected mosquitos
-muM = 1/20;      #Mosquito death rate, 1 week life span for mosquito
-BML = .2762       #transmission rate to mosquito from livestock
+muM = 1/31.5;      #Mosquito death rate, 1 week life span for mosquito
+BML = .2429      #transmission rate to mosquito from livestock
 
 #Livestock
-alphaL=(1/4);     #transition from exposed to infected livestock
-muL= (1/10/365);    #livestock death rate, life span: 10 yr
-gamma= (1/5);     # recovery rate livestock, infectious period: 7 days
+alphaL=.58;     #transition from exposed to infected livestock
+muL= (1/1980);    #livestock death rate, approx 5.5 years (average)
+gamma= .6;     # recovery rate livestock, infectious period: 7 days
 #vL=NL*muL;       #birth rate livestock
-BLM= BML           #transmission rate to livestock from mosquito
-dL = .07          #death rate in livestock due to RVF
-cul = .5
-parameters = c(cul = cul, muL = muL, muM = muM, 
+BLM= 0.2762           #transmission rate to livestock from mosquito
+dL = .0625         #death rate in livestock due to RVF
+cul = 1
+
+parameters = c(muL = muL, muM = muM, 
                gamma = gamma, alphaL = alphaL, alphaM = alphaM, 
                theta = theta, b1 = b1, q1 = q1, dL = dL, BML = BML, BLM = BLM)
 
 #state = c(SL = SL, IL = IL, SM = SM,IM = IM, EL = EL, EM = EM, P = P, Q = Q, Inci = 0)
 state = c(P=P, Q=Q, SM=SM, EM=EM, IM=IM, SL=SL, EL=EL, IL=IL, Inci=0)
 
-times=1:(365); # months
-
+times=1:(365*2); # months
 ######################
 # Simulation         #
 ######################
 sim=ode(y=state,times=times,func=SEIRMosVec,parms = parameters)
 
-
-
 ######################
 # Plots              #
 ######################
-plot(sim)
+#plot(sim)
+#df
+#new<- mutate(simdf, NM = (SM + EM + IM+P+Q))
+#plot(new$NM)
+
+simdf<- as.data.frame(sim)
+simdf2<- simdf %>%
+  rename("Uninfected Eggs" = P, "Infected Eggs" = Q, 
+         "Susceptible Mosquitos" = SM, "Exposed Mosquitos" = EM, "Infected Mosquitos" =IM,
+         "Susceptible Livestock" = SL, "Exposed Livestock" = EL, "Infectious Livestock" = IL,"Cumulative Inci" = Inci) 
+
+#simdf2melt<- simdf2 %>% select(`Susceptible Mosquitos`, `Exposed Mosquitos`,`Infected Mosquitos`, 
+#                              `Susceptible Livestock`, `Exposed Livestock`, `Infectious Livestock`, time) %>% melt(., id = 'time')
 
 
-######################
-# R0             #
-######################
+###TOTAL LIVESTOCK PLOTS
 
-((BLM*BML)/ (muM*(gamma + muL))*(NM/NL))
+#####CULLIN
+hundred<- dplyr::select(totliv, `Cumulative Inci`, time)
+hundred20<- dplyr::select(totliv20, `Cumulative Inci`)
+hundred40<- dplyr::select(totliv40, `Cumulative Inci`)
+hundred55<- dplyr::select(totliv55, `Cumulative Inci`)
+hundred77<- dplyr::select(totliv77, `Cumulative Inci`)
+
+new<- cbind(hundred, hundred20, hundred40, hundred55, hundred77)
+names(new) = c("100:128","time", "120:148", "140:168", "155:183", "177:205")
+
+new2<- melt(new, id = 'time')
+
+##CULLING
+jpeg('culling.jpeg', units="in", width=8, height=4, res=300)
+ggplot(data=new2, aes(x=time, y= value, group=variable, colour=variable)) + 
+  geom_line(size=.5) + ylim(c(0,1000)) + xlim(c(0, 750)) +ylab("Number of Livestock")
+dev.off()
 
 
+totliv77 <- simdf2 %>% 
+  select(time, `Susceptible Livestock`, `Exposed Livestock`, `Infectious Livestock`, `Cumulative Inci`)
+
+totlivmel <- totliv %>% melt(., id = 'time')
+
+#jpeg('ptotliv.jpeg', units="in", width=15, height=10, res=300)
+ptotliv<- ggplot(totlivmel, aes(time, value)) + 
+  geom_line(size = 1, color = "light green") + 
+  facet_wrap(~variable, scales="free_y") + 
+  xlab("Time (Days)") + ylab("Number of Livestock") +
+  theme_minimal() + theme(strip.text = element_text(size=15))
+ptotliv
+#dev.off()
+
+max(totliv$`Infectious Livestock`)
+
+totliv[]
 ######################
 # Term-time forcing  #
 ######################
 # term-time forcing
-holidays=c(100:128);
-times=seq(1,365); # in day, starts from 1
+holidays=c(177:205);
+times=seq(1,365*2); # in day, starts from 1
 Term=rep(0,length(times)); # intial a vector to store the Term
 # find those days that are school holidays
-ind=(1:length(Term) %% 365) %in% holidays  
+ind=(1:length(Term) %% (365*2)) %in% holidays  
 Term[ind]=1; # set them to -1
